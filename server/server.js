c20fc6734bd3321adb42ce885fb682da6a4af9bb
Rob Brennan <rob@therobbrennan.com>
2019-01-26 16:35:39 -0800

    Added server side logging with morgan

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const routes = require('../api/routes')
+const morgan = require('morgan')
 
-const PORT = 3005
+const PORT = 3005 // TODO: Define PORT as an environment variable
 
 // Setup the app
 const app = express()
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
